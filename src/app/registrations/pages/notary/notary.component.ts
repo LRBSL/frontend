2145c8ac8548fd3395afff7c92dec050622a1d09
RavinduSachintha <ravindusachintha53@gmail.com>
2020-02-19 18:17:30 +0000

    updated reg

--- src/app/registrations/pages/notary/notary.component.ts
+++ src/app/registrations/pages/notary/notary.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-notary',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotaryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, @Inject(DOCUMENT) private document: Document) { }
+
+  alert_type: string;
+  alert_content: string;
+
+  notaryRegisterForm = new FormGroup({
+    fname: new FormControl('', [Validators.required]),
+    lname: new FormControl('', [Validators.required]),
+    nic: new FormControl('', [Validators.required]),
+    regId: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
   ngOnInit() {
   }
--- src/app/registrations/pages/rlr/rlr.component.ts
+++ src/app/registrations/pages/rlr/rlr.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { DOCUMENT } from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rlr',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RlrComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, @Inject(DOCUMENT) private document: Document) { }
+
+  alert_type: string;
+  alert_content: string;
+
+  notaryRegisterForm = new FormGroup({
+    fname: new FormControl('', [Validators.required]),
+    lname: new FormControl('', [Validators.required]),
+    nic: new FormControl('', [Validators.required]),
+    regId: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
   ngOnInit() {
   }
